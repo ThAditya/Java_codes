9789cff2d6f01bcba78453e5253464475fcd82fc
Aditya Vashishth <156231205+ThAditya@users.noreply.github.com>
2024-02-08 23:51:02 +0530

    Add files via upload

---
+++ count_numbers.java
@@ -0,0 +1,24 @@
+// Write a program to enter the numbers till the user wants and at the end it
+// should display the count of positive, negative and zeros entered.
+
+import java.util.*;
+
+public class count_numbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of n: ");
+        int n = sc.nextInt();
+        int count = 0, i = 0, zero = 0, negative = 0;
+        String str = sc.next();
+        while() {
+            System.out.println(i);
+        }
+        System.out.println("Enter the value of O: ");
+        int O = sc.nextInt();
+        for (i = 0; i <= O; i++) {
+            count++;
+        }
+        System.out.println(zero);
+        System.out.println(negative);
+    }
+}
