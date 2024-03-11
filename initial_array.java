53fd6078b52f28a57090c3ab8055aa81e7329842
Aditya Vashishth <156231205+ThAditya@users.noreply.github.com>
2024-03-11 23:44:33 +0530

    Add files via upload

---
+++ initial_array.java
@@ -0,0 +1,12 @@
+
+//Program to initialize an array with given values.
+import java.util.*;
+
+public class initial_array {
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 3, 4, 5 };
+        for (int i = 0; i < 5; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
